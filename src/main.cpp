b378ebcbb2fb4061e0d1f881f152fffa09546519
Braedy Kuzma <braedy@ualberta.ca>
2020-06-22 18:24:23 -0600

    Add logging library.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
-#include "sim/InstInfo.h"
+#include "loguru.hpp"
 
 int main(int argc, char **argv) {
+  loguru::Options opts {"-v", "main", true};
+  loguru::init(argc, argv, opts);
 
+  loguru::shutdown();
   return 0;
 }
