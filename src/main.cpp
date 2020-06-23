810e6915130129d07b5a559c0b870e57753a5bd8
Braedy Kuzma <braedy@ualberta.ca>
2020-06-22 18:42:03 -0600

    Start and run sim.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
+#include "sim/Simulator.h"
+
 #include "loguru.hpp"
 
 int main(int argc, char **argv) {
   loguru::Options opts {"-v", "main", true};
   loguru::init(argc, argv, opts);
+
+  LOG_F(INFO, "Creating simulator.");
+  Simulator sim(argv[1]);
+
+  sim.run();
 
   loguru::shutdown();
   return 0;
