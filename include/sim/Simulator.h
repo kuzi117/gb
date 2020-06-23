aaf6aac5566d52997740c52973ecfae56933ec87
Braedy Kuzma <braedy@ualberta.ca>
2020-06-22 18:37:42 -0600

    Update Simulator with start up.

--- include/sim/Simulator.h
+++ include/sim/Simulator.h
@@ -4,6 +4,36 @@
 #include "sim/RegisterInfo.h"
 
 class Simulator {
+public:
+  //! Simulator must be loaded with a ROM.
+  Simulator() = delete;
+
+  //! Simulator doesn't support copy construction.
+  Simulator(const Simulator &) = delete;
+
+  /**
+   * \brief Initialise the simulator.
+   *
+   *
+   * This initialises the simulator with the ROM located at the provided
+   * filepath.
+   *
+   * \param romLoc Filepath to ROM.
+   */
+  Simulator(const char *romLoc);
+
+  //! Run the simulation.
+  void run();
+
+private:
+  //! Reset the simulator's internal state (registers, RAM, stack, etc.).
+  void reset();
+
+  /**
+   * \brief Load the ROM located at the filepath provided.
+   * \param romLoc
+   */
+  void load(const char *romLoc);
 
 private:
   //! The simulator's current program counter.
@@ -14,7 +44,6 @@ private:
 
   //! The simulator's registers.
   uint16_t regs[4];
-
 };
 
 #endif // GB_SIMULATOR_H
---
+++ src/sim/Simulator.cpp
@@ -0,0 +1,16 @@
+#include "sim/Simulator.h"
+
+Simulator::Simulator(const char *romLoc) {
+  reset();
+  load(romLoc);
+}
+
+void Simulator::reset() {
+}
+
+void Simulator::load(const char *romLoc) {
+}
+
+void Simulator::run() {
+}
+
