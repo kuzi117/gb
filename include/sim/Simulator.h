af1ca654ffea2c72300eddd52cc8ed7a0e57fc4d
Braedy Kuzma <braedy@ualberta.ca>
2020-07-03 00:00:08 -0600

    Add simulator with reset and mem controller.

--- include/sim/Simulator.h
+++ include/sim/Simulator.h
@@ -1,7 +1,17 @@
 #ifndef GB_SIMULATOR_H
 #define GB_SIMULATOR_H
 
+#include "sim/mem/MemoryController.h"
 #include "sim/RegisterInfo.h"
+
+#include <memory>
+
+namespace regdefaults {
+
+//! The default value for the 4 physical registers.
+constexpr uint16_t physRegs[4] = {0x01B0, 0x0013, 0x00D8, 0x014D};
+
+}
 
 class Simulator {
 public:
@@ -31,7 +41,7 @@ private:
 
   /**
    * \brief Load the ROM located at the filepath provided.
-   * \param romLoc
+   * \param romLoc The filepath for the ROM.
    */
   void load(const char *romLoc);
 
@@ -44,6 +54,9 @@ private:
 
   //! The simulator's registers.
   uint16_t regs[4];
+
+  //! The memory controller for the simulator, created based on ROM.
+  std::unique_ptr<MemoryController> mem;
 };
 
 #endif // GB_SIMULATOR_H
--- src/sim/Simulator.cpp
+++ src/sim/Simulator.cpp
@@ -1,11 +1,43 @@
 #include "sim/Simulator.h"
 
-Simulator::Simulator(const char *romLoc) {
-  reset();
+#include "sim/mem/MBC0.h"
+
+#include "loguru.hpp"
+
+#include <cassert>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+
+using namespace regdefaults;
+
+namespace {
+
+
+} // End anonymous namespace.
+
+// Init values to 0 so as to avoid undefined behaviour in calling member
+// functions (i.e. reset).
+Simulator::Simulator(const char *romLoc) : PC(0), SP(0), regs{0}, mem(nullptr) {
   load(romLoc);
+  reset();
 }
 
 void Simulator::reset() {
+  LOG_F(INFO, "Resetting simulator.");
+
+  // Reset registers.
+  DLOG_F(1, "Resetting physical registers.");
+  PC = 0;
+  SP = 0xFFFE;
+  std::memcpy(regs, physRegs, sizeof physRegs);
+  DLOG_F(1, "Done resetting physical registers.");
+
+  // Reset memory.
+  assert(mem != nullptr && "Memory controller was null.");
+  mem->reset();
+
+  LOG_F(INFO, "Finished resetting simulator.");
 }
 
 void Simulator::load(const char *romLoc) {
