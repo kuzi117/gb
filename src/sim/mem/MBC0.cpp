dcad57977ce1ce11e43fe8c958e8b4cf7364fb27
Braedy Kuzma <braedy@ualberta.ca>
2020-07-03 00:02:49 -0600

    Add read/write outline with messages.

--- src/sim/mem/MBC0.cpp
+++ src/sim/mem/MBC0.cpp
@@ -33,20 +33,214 @@ MBC0::MBC0(std::ifstream &rom) : MemoryController() {
 
 uint8_t MBC0::read8(uint16_t address) const {
   DLOG_F(2, "Read8: mem[0x%04X]", address);
+  // ROM0.
+  if (address < 0x4000u) {
+    LOG_F(WARNING, "Read from ROM0 unimplemented.");
+  }
+  // ROM1.
+  else if (address < 0x8000u) {
+    LOG_F(WARNING, "Read from ROM1 unimplemented.");
+  }
+  // VRAM.
+  else if (address < 0xA000u) {
+    LOG_F(WARNING, "Read from VRAM unimplemented.");
+  }
+  // ERAM.
+  else if (address < 0xC000u) {
+    LOG_F(WARNING, "Read from ERAM unimplemented.");
+  }
+  // WRAM0.
+  else if (address < 0xD000u) {
+    LOG_F(WARNING, "Read from WRAM0 unimplemented.");
+  }
+  // WRAM1.
+  else if (address < 0xE000u) {
+    LOG_F(WARNING, "Read from WRAM1 unimplemented.");
+  }
+  // Echo RAM/unused.
+  else if (address < 0xFE00u) {
+    LOG_F(WARNING, "Read from Echo RAM unimplemented.");
+  }
+  // Sprite attribute table.
+  else if (address < 0xFEA0u) {
+    LOG_F(WARNING, "Read from sprite attribute table unimplemented.");
+  }
+  // Unusable range.
+  else if (address < 0xFF00u) {
+    LOG_F(WARNING, "Read from unusable: ignored.");
+  }
+  // I/O registers.
+  else if (address < 0xFF80u) {
+
+  }
+  // HRAM.
+  else if (address < 0xFFFFu) {
+    LOG_F(WARNING, "Read from Echo RAM unimplemented.");
+  }
+  // Interrupts enable register.
+  else if (address == 0xFFFFu) {
+    LOG_F(WARNING, "Read from IE unimplemented.");
+  }
   return 0;
 }
 
 uint16_t MBC0::read16(uint16_t address) const {
   DLOG_F(2, "Read16 mem[0x%04X]", address);
+  // ROM0.
+  if (address < 0x4000u) {
+    LOG_F(WARNING, "Read from ROM0 unimplemented.");
+  }
+  // ROM1.
+  else if (address < 0x8000u) {
+    LOG_F(WARNING, "Read from ROM1 unimplemented.");
+  }
+  // VRAM.
+  else if (address < 0xA000u) {
+    LOG_F(WARNING, "Read from VRAM unimplemented.");
+  }
+  // ERAM.
+  else if (address < 0xC000u) {
+    LOG_F(WARNING, "Read from ERAM unimplemented.");
+  }
+  // WRAM0.
+  else if (address < 0xD000u) {
+    LOG_F(WARNING, "Read from WRAM0 unimplemented.");
+  }
+  // WRAM1.
+  else if (address < 0xE000u) {
+    LOG_F(WARNING, "Read from WRAM1 unimplemented.");
+  }
+  // Echo RAM/unused.
+  else if (address < 0xFE00u) {
+    LOG_F(WARNING, "Read from Echo RAM unimplemented.");
+  }
+  // Sprite attribute table.
+  else if (address < 0xFEA0u) {
+    LOG_F(WARNING, "Read from sprite attribute table unimplemented.");
+  }
+  // Unusable range.
+  else if (address < 0xFF00u) {
+    LOG_F(WARNING, "Read from unusable: ignored.");
+  }
+  // I/O registers.
+  else if (address < 0xFF80u) {
+
+  }
+  // HRAM.
+  else if (address < 0xFFFFu) {
+    LOG_F(WARNING, "Read from Echo RAM unimplemented.");
+  }
+  // Interrupts enable register.
+  else if (address == 0xFFFFu) {
+    ABORT_F("Reading 16 bit value from interrupts enable register.");
+  }
   return 0;
 }
 
 void MBC0::write(uint16_t address, uint8_t data) {
   DLOG_F(2, "Write8: mem[0x%04X] <- 0x%02X", address, data);
+  // ROM0.
+  if (address < 0x4000u) {
+    LOG_F(WARNING, "Write to ROM0: ignored.");
+  }
+  // ROM1.
+  else if (address < 0x8000u) {
+    LOG_F(WARNING, "Write to ROM1: ignored.");
+  }
+  // VRAM.
+  else if (address < 0xA000u) {
+    LOG_F(WARNING, "Write to VRAM unimplemented.");
+  }
+  // ERAM.
+  else if (address < 0xC000u) {
+    LOG_F(WARNING, "Write to ERAM unimplemented.");
+  }
+  // WRAM0.
+  else if (address < 0xD000u) {
+    LOG_F(WARNING, "Write to WRAM0 unimplemented.");
+  }
+  // WRAM1.
+  else if (address < 0xE000u) {
+    LOG_F(WARNING, "Write to WRAM1 unimplemented.");
+  }
+  // Echo RAM/unused.
+  else if (address < 0xFE00u) {
+    LOG_F(WARNING, "Write to Echo RAM unimplemented.");
+  }
+  // Sprite attribute table.
+  else if (address < 0xFEA0u) {
+    LOG_F(WARNING, "Write to sprite attribute table unimplemented.");
+  }
+  // Unusable range.
+  else if (address < 0xFF00u) {
+    LOG_F(WARNING, "Write to unusable: ignored.");
+  }
+  // I/O registers.
+  else if (address < 0xFF80u) {
+    LOG_F(WARNING, "Write to IO registers unimplemented.");
+  }
+  // HRAM.
+  else if (address < 0xFFFFu) {
+    LOG_F(WARNING, "WRITE TO Echo RAM unimplemented.");
+  }
+  // Interrupts enable register.
+  else if (address == 0xFFFFu) {
+    if (IER == nullptr)
+      IER = std::make_unique<uint8_t>();
+    *IER = data;
+  }
 }
 
 void MBC0::write(uint16_t address, uint16_t data) {
   DLOG_F(2, "Write16: mem[0x%04X] <- 0x%02X", address, data);
+  // ROM0.
+  if (address < 0x4000u) {
+    LOG_F(WARNING, "Write to ROM0: ignored.");
+  }
+  // ROM1.
+  else if (address < 0x8000u) {
+    LOG_F(WARNING, "Write to ROM1: ignored.");
+  }
+  // VRAM.
+  else if (address < 0xA000u) {
+    LOG_F(WARNING, "Write to VRAM unimplemented.");
+  }
+  // ERAM.
+  else if (address < 0xC000u) {
+    LOG_F(WARNING, "Write to ERAM unimplemented.");
+  }
+  // WRAM0.
+  else if (address < 0xD000u) {
+    LOG_F(WARNING, "Write to WRAM0 unimplemented.");
+  }
+  // WRAM1.
+  else if (address < 0xE000u) {
+    LOG_F(WARNING, "Write to WRAM1 unimplemented.");
+  }
+  // Echo RAM/unused.
+  else if (address < 0xFE00u) {
+    LOG_F(WARNING, "Write to Echo RAM unimplemented.");
+  }
+  // Sprite attribute table.
+  else if (address < 0xFEA0u) {
+    LOG_F(WARNING, "Write to sprite attribute table unimplemented.");
+  }
+  // Unusable range.
+  else if (address < 0xFF00u) {
+    LOG_F(WARNING, "Write to unusable: ignored.");
+  }
+  // I/O registers.
+  else if (address < 0xFF80u) {
+
+  }
+  // HRAM.
+  else if (address < 0xFFFFu) {
+    LOG_F(WARNING, "WRITE TO Echo RAM unimplemented.");
+  }
+  // Interrupts enable register.
+  else if (address == 0xFFFFu) {
+    ABORT_F("Writing 16 bit value to interrupts enable register.");
+  }
 }
 
 void MBC0::reset() {
