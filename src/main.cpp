d32da5419475b3bef70b28fa889684843b7b93aa
Braedy Kuzma <braedy@ualberta.ca>
2020-07-26 11:54:32 -0600

    Die with not enough arguments.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@ int main(int argc, char **argv) {
   loguru::Options opts {"-v", "main", true};
   loguru::init(argc, argv, opts);
 
+  if (argc < 2)
+    ABORT_F("Not enough arguments.");
+
   LOG_F(INFO, "Creating simulator.");
   Simulator sim(argv[1]);
 
